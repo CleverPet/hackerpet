9ed3fd714bb06aeeae559bf3653ae7f90766547f
Jelmer Tiete <jelmer@tiete.be>
2019-06-06 21:18:55 -0400

    turn off touchpads audio and light on miss

--- examples/010_MatchingTwoColors/010_MatchingTwoColors.cpp
+++ examples/010_MatchingTwoColors/010_MatchingTwoColors.cpp
@@ -262,6 +262,8 @@ bool playMatchingTwoColors(){
 
     updateTouchpadLights();
 
+    hub.SetButtonAudioEnabled(true);
+
     // Record start timestamp for performance logging
     timestamp_before = millis();
 
@@ -369,7 +371,9 @@ bool playMatchingTwoColors(){
             hub.PlayAudio(hub.AUDIO_NEGATIVE, 80);
             // give the Hub a moment to finish playing the reward sound
             yield_sleep_ms(SOUND_FOODTREAT_DELAY, false);
-
+            // turn off touchpads sound and light during time-out
+            hub.SetLights(hub.LIGHT_BTNS, 0, 0, 0);
+            hub.SetButtonAudioEnabled(false);
             yield_sleep_ms(WRONG_INTERACTION_DELAY, false);
         }
     }
